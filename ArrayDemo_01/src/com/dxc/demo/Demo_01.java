dad9edba2071099c01db777e85755efb47fe5899
ds99 <ds99@10.218.211.86>
2020-08-31 11:20:56 +0530

    version 1.1 with second line of code

--- ArrayDemo_01/src/com/dxc/demo/Demo_01.java
+++ ArrayDemo_01/src/com/dxc/demo/Demo_01.java
@@ -5,6 +5,7 @@ public class Demo_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is a simple program");
+System.out.println("the second line");
 	}
 
 }
