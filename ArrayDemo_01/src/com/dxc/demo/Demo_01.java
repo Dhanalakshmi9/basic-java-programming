62fa483d56e67ab75c979a0198f0d8679ca9f2d1
ds99 <ds99@10.218.211.86>
2020-08-31 11:16:38 +0530

    project version-1.0,with only program demo_01 with one line of code

---
+++ ArrayDemo_01/src/com/dxc/demo/Demo_01.java
@@ -0,0 +1,10 @@
+package com.dxc.demo;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is a simple program");
+	}
+
+}
