965eace1cdaa382f2c85ebdba3de085b5e55e6b2
ds99 <ds99@10.218.211.86>
2020-08-31 12:55:23 +0530

    A display message added by Dhanam-dxc

--- ArrayDemo_01/src/com/dxc/demo/Demo_01.java
+++ ArrayDemo_01/src/com/dxc/demo/Demo_01.java
@@ -8,7 +8,7 @@ System.out.println("this is a simple program");
 System.out.println("the second line");
 for(int c=0;c<10;c++) {
 	System.out.println(c);
-}
+}System.out.println("logic ends here by dhanam");
 System.out.println("thankyou for using the program");
 	}
 
